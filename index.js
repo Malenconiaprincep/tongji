861e7ebdc71016a975d83aee4944a86f36375ab7
makuta <wangbo@wepiao.com>
2016-12-05 22:35:44 +0800

    update

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function *sendMessage(phone) {
       form: {
         mobile: phone,
         apikey: 'cd9f8e6871bcc421ac689cc4d8352b5b',
-        text: '【BemCloud】您的验证码是1。如非本人操作，请忽略本短信1111'
+        text: '【BemCloud】您的验证码是1。1111'
       }
     });
     return res
